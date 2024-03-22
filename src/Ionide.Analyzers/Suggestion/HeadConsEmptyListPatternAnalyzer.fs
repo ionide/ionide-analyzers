6b2d69b512e928d60c858dec4fd70b4c9c98a2df
nojaf <florian.verdonck@outlook.com>
2024-03-22 11:07:47 +0100

    Add code fix

--- src/Ionide.Analyzers/Suggestion/HeadConsEmptyListPatternAnalyzer.fs
+++ src/Ionide.Analyzers/Suggestion/HeadConsEmptyListPatternAnalyzer.fs
@@ -7,31 +7,49 @@ open FSharp.Compiler.Text
 open FSharp.Analyzers.SDK.ASTCollecting
 
 [<Literal>]
-let message = "Replace `[ x ] :: _` with `[ x ]`"
+let message = "Replace `x :: _` with `[ x ]`"
 
-let private analyze (parsedInput: ParsedInput) =
-    let patterns = HashSet<range>(Range.comparer)
+let private analyze (sourceText: ISourceText) (parsedInput: ParsedInput) =
+    let patterns = HashSet<range * string option>()
 
     let collector =
         { new SyntaxCollectorBase() with
             override x.WalkPat(path, synPat) =
                 match synPat with
-                | SynPat.ListCons(rhsPat = SynPat.ArrayOrList(isArray = false; elementPats = [])) ->
-                    patterns.Add synPat.Range |> ignore
+                | SynPat.ListCons(lhsPat = lhsPat; rhsPat = SynPat.ArrayOrList(isArray = false; elementPats = [])) ->
+                    let text =
+                        if lhsPat.Range.StartLine <> lhsPat.Range.EndLine then
+                            None
+                        else
+                            Some($"[ %s{sourceText.GetSubTextFromRange(lhsPat.Range)} ]")
+
+                    patterns.Add(synPat.Range, text) |> ignore
                 | _ -> ()
         }
 
     walkAst collector parsedInput
 
     patterns
-    |> Seq.map (fun mPat ->
+    |> Seq.map (fun (mPat, fixText) ->
+        let fixes =
+            match fixText with
+            | None -> []
+            | Some fixText ->
+                [
+                    {
+                        FromText = ""
+                        FromRange = mPat
+                        ToText = fixText
+                    }
+                ]
+
         {
             Type = "HeadConsEmptyListPattern analyzer"
             Message = message
             Code = "IONIDE-007"
             Severity = Severity.Hint
             Range = mPat
-            Fixes = []
+            Fixes = fixes
         }
     )
     |> Seq.toList
@@ -47,8 +65,8 @@ let helpUri = "https://ionide.io/ionide-analyzers/suggestion/007.html"
 
 [<CliAnalyzer(name, shortDescription, helpUri)>]
 let headConsEmptyListPatternCliAnalyzer (ctx: CliContext) =
-    async { return analyze ctx.ParseFileResults.ParseTree }
+    async { return analyze ctx.SourceText ctx.ParseFileResults.ParseTree }
 
 [<EditorAnalyzer(name, shortDescription, helpUri)>]
 let headConsEmptyListPatternEditorAnalyzer (ctx: EditorContext) =
-    async { return ctx.ParseFileResults.ParseTree }
+    async { return analyze ctx.SourceText ctx.ParseFileResults.ParseTree }
